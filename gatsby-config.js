22d0c593ebd978e41b1e993bc7f7d345490f0bc2
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-02-17 17:44:53 +0900

    🧚🏻‍♀️ chore: update bio and profile pic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `lemon-Sour fm`,
+    author: `DJ lemon-Sour`,
+    description: `A Podcast by DJ lemon-Sour.`,
+    siteUrl: `https://lemon-sour-fm.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `hisasann`,
     },
   },
   plugins: [
